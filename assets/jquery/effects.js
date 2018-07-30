d616ba3e6a76e0b0446c713460b872ef706c088d
Khari Woods <khariwoods3@gmail.com>
2018-07-29 20:14:23 -0400

    Content Modified - changes should be final

--- assets/jquery/effects.js
+++ assets/jquery/effects.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 		// Correct code below
-
 		$('.box').hover(
 			function(){$(this).find('.summary:first').fadeIn(500);},
 			function(){$(this).find('.summary:first').fadeOut(500);}
